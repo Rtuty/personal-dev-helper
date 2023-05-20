85b35427c74560cd0e9eb4f4e75fa9ecfd595284
Kudryavtsev Kirill <rtutiui@mail.ru>
2023-05-20 15:53:54 +0300

    first commit

---
+++ go.mod
@@ -0,0 +1,8 @@
+module dev-helper
+
+go 1.18
+
+require (
+	github.com/go-telegram/bot v0.7.9
+	github.com/joho/godotenv v1.5.1
+)
---
+++ main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"log"
+	"os"
+	"os/signal"
+
+	"github.com/go-telegram/bot"
+	"github.com/go-telegram/bot/models"
+	"github.com/joho/godotenv"
+)
+
+func init() {
+	if err := godotenv.Load(); err != nil {
+		log.Print("No .env file found")
+	}
+}
+
+func initBotToken() (string, error) {
+	token, exists := os.LookupEnv("APITOKEN")
+	if !exists {
+		return "", errors.New("bot token is not found")
+	}
+
+	return token, nil
+}
+func main() {
+	token, err := initBotToken()
+	if err != nil {
+		panic(err)
+	}
+
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer cancel()
+
+	opts := []bot.Option{
+		bot.WithDefaultHandler(handler),
+	}
+
+	b, err := bot.New(token, opts...)
+	if err != nil {
+		panic(err)
+	}
+
+	b.Start(ctx)
+}
+
+func handler(ctx context.Context, b *bot.Bot, update *models.Update) {
+	b.SendMessage(ctx, &bot.SendMessageParams{
+		ChatID: update.Message.Chat.ID,
+		Text:   update.Message.Text,
+	})
+}
